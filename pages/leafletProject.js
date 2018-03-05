7f5f9bd36439caed331e235a06ed1e42617e0c5a
JJ <jleopold@wisc.edu>
2018-03-04 22:30:32 -0700

    updated pages folder

--- pages/leafletProject.js
+++ pages/leafletProject.js
@@ -8,9 +8,9 @@ var map = L.map('map',{
     zoomControl: false,
 })
 
-var pixar = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
+var pixar = L.tileLayer('https://api.mapbox.com/styles/v1/jleopold/cjd303coe3wkh2rl0zoezvy8o/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiamxlb3BvbGQiLCJhIjoiY2l5MXV2ZDIzMDAwMTMycGdxYnMwbTVvZiJ9.u54u0PD7k942ESruEVc8rg',{
+    minZoom: 1,
+    maxZoom: 22
 }).addTo(map);
 
 var place = L.marker([47, -114]).addTo(map);
@@ -29,3 +29,16 @@ var polygon = L.polygon(latlngs, {color: '#00D7FF', fillColor: "#00D7FF", opacit
 
 var latlngs = [[48.240130, -114.352044],[48.240246, -114.351756],[48.240250, -114.345007],[48.240050, -114.343690],[48.239650, -114.342800],[48.239297, -114.342319],[48.238492, -114.341759],[48.236977, -114.341669],[48.236028, -114.345494],[48.238240, -114.350675],[48.238250, -114.352044]];
 var polygon = L.polygon(latlngs, {color: '#00D7FF', fillColor: "#00D7FF", opacity: 1, fillOpacity: .33}).addTo(map);
+    
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}.addTo(map);
