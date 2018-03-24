2f7dee184395856799ef9dfcfd84fafa8ee90e88
JJ <jleopold@wisc.edu>
2018-03-24 02:16:30 -0600

    Added pip
    Point-In-Polygon app

---
+++ bootstrap/css/locations.js
@@ -0,0 +1,21 @@
+var locationsData = {"type":"FeatureCollection","features":[
+    
+{"type":"Feature","properties":{"name":"Washington Monument"},"geometry":{"type":"Polygon","coordinates":[[[-77.035707,38.889820],[-77.034815,38.889820],[-77.034815,38.889144],[-77.035707,38.889144]]]}},
+    
+{"type":"Feature","id":"1","properties":{"name":"The Leopold's House","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.305778,48.304584],[-114.305190,48.304584],[-114.305190,48.304100],[-114.305778,48.304100]]]}},
+    
+{"type":"Feature","id":"2","properties":{"name":"Glacier High School","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.352044,48.240130],[-114.351756,48.240246],[-114.345007,48.240250],[-114.343690,48.240050],[-114.342800,48.239650],[-114.342319,48.239297],[-114.341759,48.238492],[-114.341669,48.236977],[-114.345494,48.236028],[-114.350675,48.238240],[-114.352044,48.238250]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"Starbucks","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.331670,48.237137],[-114.331040,48.237137],[-114.331040,48.236600],[-114.331670,48.236600]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"Silverbrook","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.346406,48.283810],[-114.330832,48.283810],[-114.330832,48.273139],[-114.346406,48.273139]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"The Summit Medical Fitness Center","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.328134,48.217002],[-114.326191,48.216851],[-114.326191,48.215200],[-114.327698,48.215240],[-114.327630,48.216224],[-114.328224,48.216303]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"Signature Theatres","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.327482,48.234903],[-114.325982,48.234903],[-114.325982,48.233592],[-114.327482,48.233592]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"Glacier Memorial Gardens","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.330232,48.247673],[-114.326407,48.247673],[-114.326407,48.245552],[-114.330232,48.245552]]]}},
+    
+{"type":"Feature","id":"6","properties":{"name":"Kalispell Ford","density":1082 },"geometry":{"type":"Polygon","coordinates":[[[-114.330234,48.254881],[-114.327641,48.254881],[-114.327641,48.253568],[-114.330234,48.253568]]]}},
+    
+]};
